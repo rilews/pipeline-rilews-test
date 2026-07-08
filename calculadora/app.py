7d326061f83aea24b97c7e0b6f198a7aebc7c04a
Bayron <b.munoz15@ufromail.cl>
2026-07-08 16:03:28 -0400

    feat: añade job analisis código con sonnar

--- calculadora/app.py
+++ calculadora/app.py
@@ -52,4 +52,5 @@ def health():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", "5000"))
-    app.run(host="0.0.0.0", port=port)
+    host = os.getenv("HOST", "127.0.0.1")
+    app.run(host=host, port=port)
